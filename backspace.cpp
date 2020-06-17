26b4434efb8112fe143df8318d313331b650a18d
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-06-16 23:49:42 -0400

    some simple programs

---
+++ backspace.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "\b Hello" << std::endl; 
+    return 0;
+}
---
+++ tabs.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Say something here. \t Tabbed Say something here \n \t Say something else." << std::endl; 
+    return 0;
+}
