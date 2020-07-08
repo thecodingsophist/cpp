89c02635b52ee967d4c56104c0318ec3d061b027
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-07-08 11:07:21 -0400

    q/another_name.cpp

---
+++ AcceleratedCpp/2_/another_name.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "What is your name? ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name
+              << std::endl << "And what is yours? ";
+    std::cin >> name;
+    std::cout << "Hello, " << name
+              << "; nice to meet you too" << std::endl;
+    return 0;
+}
+
+//TODO: Figure out why when you type something with a space that it registers as two lines
